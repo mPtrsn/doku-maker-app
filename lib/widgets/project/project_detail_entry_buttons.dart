843833decd48ce8db6345d279c1087a27cd0342a
mPtrsn <malte.ptrsn@gmail.com>
2021-03-23 23:16:12 +0100

    []
    dev: UX updates to search function
    feats: -
    bug: -

--- lib/widgets/project/project_detail_entry_buttons.dart
+++ lib/widgets/project/project_detail_entry_buttons.dart
@@ -51,69 +51,94 @@ class _ProjectDetailEntryButtonsState extends State<ProjectDetailEntryButtons> {
   Widget build(BuildContext context) {
     return Container(
       height: 80,
-      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          if (searchMode == SearchMode.None)
-            _buildEntryButton(context, Icons.title, () {
-              showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (ctx) => NewTextEntryModal(widget.project.id));
-            }),
-          if (searchMode == SearchMode.None)
-            _buildEntryButton(context, Icons.image, () {
-              showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (ctx) => NewImageEntryModal(widget.project.id));
-            }),
-          if (searchMode == SearchMode.None)
-            _buildEntryButton(context, Icons.play_arrow, () {
-              showModalBottomSheet(
-                  context: context,
-                  isScrollControlled: true,
-                  backgroundColor: Colors.transparent,
-                  builder: (ctx) => NewVideoEntryModal(widget.project.id));
-            }),
-          if (searchMode == SearchMode.Searching)
-            Expanded(
-              child: Form(
-                key: _formKey,
-                child: TextFormField(
-                  autofocus: true,
-                  decoration: InputDecoration(labelText: 'Search'),
-                  textInputAction: TextInputAction.search,
-                  onChanged: searchChanged,
-                ),
-              ),
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      child: searchMode == SearchMode.None
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildEntryButton(context, Icons.title, () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (ctx) => NewTextEntryModal(widget.project.id));
+                }),
+                _buildEntryButton(context, Icons.image, () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (ctx) => NewImageEntryModal(widget.project.id));
+                }),
+                _buildEntryButton(context, Icons.play_arrow, () {
+                  showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true,
+                      backgroundColor: Colors.transparent,
+                      builder: (ctx) => NewVideoEntryModal(widget.project.id));
+                }),
+                _buildEntryButton(context, Icons.search, () {
+                  setState(() {
+                    searchMode = SearchMode.Searching;
+                  });
+                }, Theme.of(context).buttonColor),
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (searchMode == SearchMode.Searching)
+                  Expanded(
+                    child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        autofocus: true,
+                        decoration: InputDecoration(labelText: 'Search'),
+                        textInputAction: TextInputAction.search,
+                        onChanged: searchChanged,
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            searchMode = SearchMode.Results;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                if (searchMode == SearchMode.Results)
+                  Column(
+                    children: [
+                      Text(
+                        'Search',
+                        style: TextStyle(fontSize: 11),
+                      ),
+                      Text(
+                        _searchString,
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ],
+                  ),
+                _buildEntryButton(
+                    context,
+                    searchMode == SearchMode.Results
+                        ? Icons.close
+                        : Icons.search, () {
+                  if (searchMode == SearchMode.Searching) {
+                    setState(() {
+                      searchMode =
+                          (_searchString != null && _searchString.isNotEmpty)
+                              ? SearchMode.Results
+                              : SearchMode.None;
+                    });
+                  } else if (searchMode == SearchMode.Results) {
+                    setState(() {
+                      searchMode = SearchMode.None;
+                    });
+                    searchChanged('');
+                  }
+                }, Theme.of(context).buttonColor),
+              ],
             ),
-          if (searchMode == SearchMode.Results) Text('Search: $_searchString'),
-          _buildEntryButton(context,
-              searchMode == SearchMode.Results ? Icons.close : Icons.search,
-              () {
-            if (searchMode == SearchMode.None) {
-              setState(() {
-                searchMode = SearchMode.Searching;
-              });
-            } else if (searchMode == SearchMode.Searching) {
-              setState(() {
-                searchMode = (_searchString != null && _searchString.isNotEmpty)
-                    ? SearchMode.Results
-                    : SearchMode.None;
-              });
-            } else if (searchMode == SearchMode.Results) {
-              setState(() {
-                searchMode = SearchMode.None;
-              });
-              searchChanged('');
-            }
-          }, Theme.of(context).buttonColor),
-        ],
-      ),
     );
   }
 }
